3eb1903a3600e47e2ac1bb07feb6fae29bca5c75
PabloNahra <pnahra@satrendy.com>
2023-07-09 22:56:11 -0300

    primer commit

--- carrito.js
+++ carrito.js
@@ -39,6 +39,7 @@ function listar_productos(){
 
 function pedir_datos_cliente(){
     nombre_y_apellido = prompt('Indique su nombre y apellido');
+    domicilio = prompt('Indique su domicilio de entrega');
 }
 
 function cargar_productos(){
@@ -65,12 +66,55 @@ function cargar_productos(){
     return producto, precio
 }
 
+/// traemos del localStorage
+function tomarCarrito() {
+    carrito = JSON.parse(localStorage.getItem('carrito'));
+}
+
+function dibujarTablaItems() {
+    const bodyTabla = document.getElementById('items');
+    bodyTabla.innerHTML = ``; // limpio datos de la tabla
+    // recorremos el array del carrito
+    carrito.forEach((item,index) => {
+       let tr = document.createElement('tr'); ///creo una fila!
+       let th = document.createElement('th'); ///creo el header de la fila!
+       th.textContent = index+1;
+       tr.appendChild(th); ///le agrego el th como hijito de la fila
+
+       /////creo la celda para el nombre de item
+       let td = document.createElement('td');
+       td.textContent = item.producto;
+       tr.appendChild(td); ///agrego un nuevo hijito con el nombre de item a la fila
+
+       ///cantidad
+       td = document.createElement('td');
+       td.textContent = item.cantidad;
+       tr.appendChild(td);
+
+       ///precio unitario
+       td = document.createElement('td');
+       td.textContent = item.precio;
+       tr.appendChild(td);
+
+       // total_item
+       td = document.createElement('td');
+       td.textContent = item.precio * item.cantidad;
+       tr.appendChild(td);
+
+
+       ///hago la row hija del body de la tabla
+       bodyTabla.appendChild(tr); 
+    });
+}
 
 // Comienzo con el programa
 alert("Bienvenido al proceso de compra")
 
-//Solicito datos al cliente
-pedir_datos_cliente();
+// Limpio el Storage al comenzar
+localStorage.clear()
+
+// defino el carrito
+var carrito = [];
 
 do {
     // Solicito producto al usuarios
@@ -82,9 +126,41 @@ do {
     // Sumo al total de la compra
     total_compra = total_compra + (precio * cantidad);
 
+    // (3) Enviar los valores del producto al storage
+    localStorage.setItem('producto', producto)
+    localStorage.setItem('cantidad', cantidad)
+    localStorage.setItem('precio', precio)
+    localStorage.setItem('total_compra', total_compra)
+
+    nuevo_item = {'producto': producto, 'cantidad': cantidad, 'precio': precio}
+    carrito.push(nuevo_item)
+
     salir = prompt('Desea agregar otro producto en el carrito de compras? SI/NO');
+
 
 }while(salir.toUpperCase() != 'NO');
 
-// Le muestro al cliente el total de su compras
-alert(nombre_y_apellido.toUpperCase() + " el total de tu compra fue de $ " + total_compra) 
+// (3) Guardar los datos en el storage
+//console.log(carrito)
+localStorage.setItem('carrito', JSON.stringify(carrito))
+
+// (3) Tomar los valores de la compra del storage para cargar una tabla 
+tomarCarrito();
+dibujarTablaItems();
+
+// Al realizar el click de confirmar compra se dispara el proceso de confirmación
+let boton_enviar = document.getElementsByClassName("boton-envio")[0]
+
+boton_enviar.addEventListener("click", function() {
+    //Solicito datos al cliente
+    pedir_datos_cliente();
+
+    // Le muestro al cliente el total de su compras
+    //alert(nombre_y_apellido.toUpperCase() + " el total de tu compra fue de $ " + total_compra) 
+
+    // Cambio texto y color del botón
+    //boton_enviar.value = "Compra confirmada por el total de $ xxxx"; // Modifica el valor del atributo "value" al hacer clic en el botón
+    boton_enviar.value = nombre_y_apellido.toUpperCase() + " su pedido fue confirmado; el total de tu compra fue de $ " + total_compra; // Modifica el valor del atributo "value" al hacer clic en el botón
+    boton_enviar.classList.add("clicked"); // Agrega la clase "clicked" al botón al hacer clic
+});
+
